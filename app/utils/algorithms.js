a12ebfd079a46249af88c915fc130ce1a8c1b3e3
Naseem Alnaji <nalnaji@berkeley.edu>
2017-03-27 21:54:38 -0700

    feat: first draft of algorithm. Not tested, but should work in theory

--- app/utils/algorithms.js
+++ app/utils/algorithms.js
@@ -1,3 +1,76 @@
+// Does a simple algorithm: Selects initial people at random then replaces people 
+// to see if they improve the score. It saves the best replacement for each person
+var selectFocusGroup = ( people, constraints) => {
+  let size = getFocusGroupSize(constraints)
+  if(size > people)
+    return null
+
+  var focusGroup = grabRandomFocusGroup(people, size)
+
+  for (i=0; i<people.length; i++) {
+    focuGroup = tryPersonInGroup(i, people, focusGroup, constraints)
+  }
+
+  return focusGroup.values()
+}
+
+// Tries to see if a person is a better fit in the group. Finds its optimal swap
+// and replaces that person. If no swap, it returns the same group.
+//
+// Returns a focus group object.
+var tryPersonInGroup = ( id, people, group, constraints ) => {
+  // Person is in group already
+  if(group[id] != null)
+    return group
+  
+  var minId = null
+  var minScore = scoreDataset(group.values(), constraints)
+  
+  for (var personId in group) {
+    if (group.hasOwnProperty(personId)) {
+      var hold = group[personId]
+      group[personId] = people[id]  
+
+      // Try to see score replacing each person
+      var score = scoreDataset(group.values(), constraints)
+      group[personId] = hold
+
+      // Find the best replacement
+      if(score < minScore) {
+        minId = personId
+        minScore = score
+      }
+    }
+  }
+
+  if(minId != null) {
+    delete group[minId]
+    group[id] = people[id]
+  }
+
+  return group
+}
+
+// Returns a JS Object with random people using their 'index' as keys to identify them
+var grabRandomFocusGroup = ( people, size ) => {
+  var grabbed = {}
+  while(grabbed.length < size) {
+    var index = getRandomInt(0, people.length)
+    if(grabbed[index] != null)
+      grabbed[index] = people[index]
+  }
+
+  return grabbed
+}
+
+var getFocusGroupSize = ( constraints ) => {
+  var size = 0
+  for (let constraint of constraints)
+    size += constraint.count
+
+  return size
+}
+
 // Returns the score of a set of people and how closely they follow
 // a set of constraints.
 // People Array: [ {age: "10-20", ...} ]
@@ -13,7 +86,8 @@ var scoreDataset = ( people, constraints ) => {
 }
 
 
-// Returns the score for a given constraint
+// Returns the score for a given constraint, this is defined as the difference 
+// between actual matches to a category and expected matches.
 var scoreConstraint = ( people, constraint ) => {
   var matches = 0
 
---
+++ app/utils/helper.js
@@ -0,0 +1,6 @@
+var getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
