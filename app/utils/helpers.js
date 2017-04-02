7a9245a14b61909152dfc3a437e58bad73a2ac9a
Naseem Alnaji <nalnaji@berkeley.edu>
2017-04-02 14:19:28 -0700

    fix for helpers

--- app/utils/helpers.js
+++ app/utils/helpers.js
@@ -1,8 +1,8 @@
 const capitalize = (text) => `${text.charAt(0).toUpperCase()}${text.substr(1)}`;
 
 const getRandomInt = (min, max) => {
-  let min = Math.ceil(min);
-  let max = Math.floor(max);
+  min = Math.ceil(min);
+  max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
